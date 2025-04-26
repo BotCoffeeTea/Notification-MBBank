6f6e7cdde9586cc09fe4bf2779c332f824f0d9c2
Bột Coffee & Tea <113244777+BotCoffeeTea@users.noreply.github.com>
2025-04-26 21:42:05 +0700

    Update MainActivity.kt

--- app/src/main/java/com/moneyreader/MainActivity.kt
+++ app/src/main/java/com/moneyreader/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.moneyreader
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnSettings).setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 }
